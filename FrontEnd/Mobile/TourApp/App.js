2ebe3be8d064c6b98f94743cd4352e18242efc2e
BaoTruc <tranbaotruc2002bc@gmail.com>
2024-09-20 02:37:08 +0700

    Mobile: UI Home_DetailTour_p1

--- FrontEnd/Mobile/TourApp/App.js
+++ FrontEnd/Mobile/TourApp/App.js
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { NavigationContainer } from "@react-navigation/native";
+import Toast from "react-native-toast-message";
+import Main from "./src/screens/RootComponent"
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer >
+      <Main/>
+      <Toast />
+    </NavigationContainer>
+
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
