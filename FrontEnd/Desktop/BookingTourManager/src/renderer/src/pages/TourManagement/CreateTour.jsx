db6d72bbe02a320713c9fde24650d8eae7fea5ce
BaoTruc <tranbaotruc2002bc@gmail.com>
2024-11-04 21:28:23 +0700

    update desktop: create project

---
+++ FrontEnd/Desktop/BookingTourManager/src/renderer/src/pages/TourManagement/CreateTour.jsx
@@ -0,0 +1,13 @@
+import { useEffect, useRef, useState } from "react";
+
+function CreateTour() {
+  return (
+    <>
+      <div className="w-full h-full flex flex-col">
+
+      </div>
+    </>
+  );
+}
+
+export default CreateTour;
